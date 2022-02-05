a5e7999ed9c1d1ceb5564680f81e0c0039acd3ff
Alex Cebulski <alexcebulski@gmail.com>
2022-02-05 16:50:36 -0700

    edits

--- scripts/plot-2k-network.R
+++ scripts/plot-2k-network.R
@@ -6,19 +6,85 @@
 library(lipdR)
 library(tidyverse)
 
-l
 
 #nam <- readLipd() 
 #saveRDS(nam, 'nam.rds')
 
-nam <- readRDS('nam.rds') 
+nam <- readRDS('2k-network/NAm/nam.rds') 
 
-tst <- nam$`NAm-AlmondButterLower.D'Arrigo.2005`
+##### plot good tree data ####
+select_tree_sites <- c('NAm-UpperWrightLakes.Graumlich.2005', 
+                       'NAm-YellowMountainRidge.King.2002', 
+                       'NAm-FrenchGlacier.Colenutt.1995', 
+                       'NAm-Bennington.Luckman.2013', 
+                       'NAm-SmallRiver.Luckman.2001', 
+                       'NAm-Athabasca.Schweingruber.1996', 
+                       'NAm-BellMountain.Schweingruber.1996')
 
-df <- as.data.frame(do.call(rbind, nam))
+select_lake_site <- c('NAm-HellsKitchenLake.Gajewski.1988', 'NAm-GreenLake.Menounos.2006')
 
-fltr <- filterTs(nam, "archiveType == lake sediment")
+tbl <- tibble(name = nam) %>% 
+  unnest_wider(name) %>% 
+  filter(dataSetName %in% select_tree_sites) 
 
-df2 <- as.data.frame(do.call(rbind, fltr))
+init <- data.frame(
+  name =  tbl$dataSetName[1],
+  year = tbl$paleoData[[1]][[1]]$measurementTable[[1]]$year$values,
+  #rw = dat[[1]]$measurementTable[[1]]$ringWidth$values,
+  value = tbl$paleoData[[1]][[1]]$measurementTable[[1]]$trsgi$values)
 
+for (i in 1:length(select_tree_sites)-1) {
+  
+    bind <- data.frame(
+      name =  tbl$dataSetName[i+1],
+      year = tbl$paleoData[[i+1]][[1]]$measurementTable[[1]]$year$values,
+      #rw = dat[[1]]$measurementTable[[1]]$ringWidth$values,
+      value = tbl$paleoData[[i+1]][[1]]$measurementTable[[1]]$trsgi$values) 
+    
+    init <- rbind(init, bind)
+}
 
+init <- init %>% mutate(
+  proxy = "tree-ring"
+)
+
+init %>% 
+  ggplot(aes(x = year, y = value, colour = name, group = name)) + 
+  geom_line()
+
+plotly::ggplotly()
+
+#### plot lake data ####
+
+select_lake_site <- c('NAm-HellsKitchenLake.Gajewski.1988', 'NAm-GreenLake.Menounos.2006')
+
+tbl <- tibble(name = nam) %>% 
+  unnest_wider(name) %>% 
+  filter(dataSetName %in% select_lake_site) 
+
+greenLake <- data.frame(
+  name =  tbl$dataSetName[1],
+  year = tbl$paleoData[[1]][[1]]$measurementTable[[1]]$year$values,
+  #rw = dat[[1]]$measurementTable[[1]]$ringWidth$values,
+  value = tbl$paleoData[[1]][[1]]$measurementTable[[1]]$thickness$values,
+  proxy = 'varve-thickness')
+
+hellsLake <- data.frame(
+  name =  tbl$dataSetName[2],
+  year = tbl$paleoData[[2]][[1]]$measurementTable[[1]]$year$values,
+  #rw = dat[[1]]$measurementTable[[1]]$ringWidth$values,
+  value = tbl$paleoData[[2]][[1]]$measurementTable[[1]]$temperature$values,
+  proxy = 'pollen')
+
+lakes <- rbind(greenLake, hellsLake)
+
+all <- rbind(init, lakes)
+
+all %>% 
+  ggplot(aes(x = year, y = value, colour = name, group = name)) + 
+  # geom_hline(yintercept = 0) +
+ # geom_smooth(method = "lm", formula = y ~ 1, colour = "black", se=F, linetype="dashed") +
+  facet_wrap(~proxy, nrow = 3, scale = 'free_y')+
+  geom_line()
+
+plotly::ggplotly()
