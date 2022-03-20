990c7c470546f510b28d0bfeb80916968f16e29f
Alex Cebulski <alexcebulski@gmail.com>
2022-03-20 13:51:05 -0600

    update to work with raw data from excel sheet and calculate core stats

--- scripts/grain-size.R
+++ scripts/grain-size.R
@@ -4,6 +4,47 @@ library(plotly)
 library(gridExtra)
 library(tidyr)
 
+v1_tb <- c(44, 124, 120, 196, 251, 257)
+v2_tb <- c(88, 92, 168, 172, 224, 228)
+
+v2_discard <- c(120)
+
+# v1_xl <- readxl::read_xlsx('data/Sediment/Grain Size/CB17_Jan_GranSize_mar23.xlsx',sheet = '226_Raw') %>% 
+#   filter(stringr::str_starts(`Sample Name`, 'Average')) %>% 
+#   mutate(core = 'V1')
+# 
+# v2_xl <- readxl::read_xlsx('data/Sediment/Grain Size/CB17_Jan_GranSize_mar23.xlsx',sheet = '224_Raw') %>% 
+#   filter(stringr::str_starts(`Sample Name`, 'Average'),
+#          !stringr::str_detect(`Sample Name`, 'Sonic')) %>% 
+#   mutate(core = 'V2')
+# 
+# gs <- rbind(v1_xl, v2_xl)
+# 
+# strs <- 
+#   'Average of \'CB17_V2_|Average of \'CB17_V1_|Average of \'CB17_V2B_|Average of \'CB17_V2C_|cm|\''
+# 
+# gs$core_depth <- gsub(strs, "", gs$`Sample Name`)
+# 
+# tb <- gs %>% 
+#   filter(stringr::str_detect(core_depth, 'Flood') | stringr::str_detect(core_depth, 'flood')) 
+# 
+# tb$core_depth <- gsub('_.*', "", tb$core_depth)
+# 
+# tb$core_depth[1] <- 12 
+# 
+# gs_no_tb <- gs %>% 
+#   filter(!stringr::str_detect(core_depth, '[F]'),
+#          !stringr::str_detect(core_depth, '[f]')
+#          ) 
+# 
+# gs_no_tb$core_depth <- gsub('.*_', "", gs_no_tb$core_depth)
+# 
+# 
+# gs_new <- rbind(gs_no_tb, tb) %>% 
+#   mutate(core_depth = as.numeric(core_depth))
+
+# saveRDS(gs_new, 'data/long_cores/gain_size_w_floods.rds')
+
 ## carbon dates ##
 # A small twig from V1 at 347 cm results in a date of 1899-1819 cal BP. 
 # A 4 cm long twig from V2 at 222 cm results in a date of 490-316 cal BP. 
@@ -27,20 +68,21 @@ ams_df <- tibble(
   depth = c(0, 347, 0, 222, 0, v2b_depth),
   ams_sample = c('V1', 'V1', 'V2a', 'V2a', 'V2b', 'V2b')
 )
-v1 <- read.csv("data/Sediment/Grain Size/CB17_Jan_GranSize_mar23_v1_noFloods.csv") %>% 
-  rename(depth = Depth..cm., 
-         year = Year..AD., 
-         D50 = Dx..50., 
-         perc_clay = Result.In.Range....01.2...m, 
-         perc_silt = Result.In.Range...2.63...m, 
-         perc_sand = Result.In.Range...63.2000...m) %>% 
-  filter(depth != 51) %>% 
-  mutate(year_bp = abs(year - 2017))
+v1 <- readRDS("data/long_cores/gain_size_w_floods.rds") %>% 
+  filter(core == 'V1') %>% 
+  rename(depth = core_depth, 
+         D50 = `Dx (50)`, 
+         perc_clay = `Result In Range  (0.01,2) μm`, 
+         perc_silt = `Result In Range  (2,63) μm`, 
+         perc_sand = `Result In Range  (63,2000) μm`) 
+# %>% 
+#   filter(depth != 51)
 
 v1_D50 <- v1 %>% 
-  select(depth, year, D50) %>% 
+  filter(!`Record Number` %in% v1_tb) %>% 
+  select(depth, D50) %>% 
   group_by(depth) %>% 
-  summarize(across(year:D50, mean)) %>% # averge double samples that were done to confirm smaller grain size at 259 cm
+  summarize(D50 = mean(D50)) %>% # averge double samples that were done to confirm smaller grain size at 259 cm
   ungroup() %>% 
   pivot_longer(D50, names_to = "group", values_to = "D50") %>% 
   mutate(year_bp_new = depth * (v1_C14$year/(v1_C14$depth_cm)),
@@ -48,9 +90,10 @@ v1_D50 <- v1 %>%
          diff_time = lag(year_ce_new) - year_ce_new)  # linear interpolation on AMS date
 
 v1_percentages <- v1 %>% 
-  select(depth, year_bp, perc_clay:perc_sand) %>% 
+  filter(!`Record Number` %in% v1_tb) %>% 
+  select(depth, perc_clay:perc_sand) %>% 
   group_by(depth) %>% 
-  summarize(across(year_bp:perc_sand, mean)) %>%  # averge double samples that were done to confirm smaller grain size at 259 cm
+  summarize(across(perc_clay:perc_sand, mean)) %>%  # averge double samples that were done to confirm smaller grain size at 259 cm
   ungroup() %>% 
   pivot_longer(perc_clay:perc_sand, names_to = "group", values_to = "perc") %>% 
   mutate(year_bp_new = depth * (v1_C14$year/(v1_C14$depth_cm)),
@@ -99,37 +142,29 @@ ggsave("figs/grain-size/v1_D50_particlePercent.png", p,  width = 6, height = 8)
 
 ##### V2 #####
 
-v2 <- read.csv("data/Sediment/Grain Size/CB17_Jan_GranSize_mar23_v2_noFloods.csv")%>% 
-  rename(depth = Depth..cm., 
-         year = Year, 
-         D50 = Dx..50., 
-         perc_clay = Result.In.Range....01.2...m, 
-         perc_silt = Result.In.Range...2.63...m, 
-         perc_sand = Result.In.Range...63.2000...m) %>% 
-  mutate(year_bp = abs(year - 2017)) %>% 
-  filter(notes != "skewed by floods on either side")
-
-v2 %>% 
-  pivot_longer(c(D50, perc_clay:perc_sand)) %>% 
-  ggplot(aes(year_bp, x = value)) +
-  geom_point() +
-  facet_grid(~name, scales = 'free') +
-  xlab("D50 (μm)") +
-  scale_y_continuous(trans = "reverse") +
-  ylab("Year (CE)") 
+v2 <- readRDS("data/long_cores/gain_size_w_floods.rds") %>% 
+  filter(core == 'V2') %>% 
+  rename(depth = core_depth, 
+         D50 = `Dx (50)`, 
+         perc_clay = `Result In Range  (0.01,2) μm`, 
+         perc_silt = `Result In Range  (2,63) μm`, 
+         perc_sand = `Result In Range  (63,2000) μm`) %>% 
+  filter(`Record Number` != 120) # skewed by floods either side
 
 v2_D50 <- v2 %>% 
-  select(depth, year_bp, D50) %>% 
+  filter(!`Record Number` %in% v2_tb) %>% 
+  select(depth, D50) %>% 
   group_by(depth) %>% 
-  summarize(across(year_bp:D50, mean)) %>% # averge double samples that were done to confirm smaller grain size at 259 cm
+  summarize(D50 = mean(D50)) %>% # averge double samples that were done to confirm smaller grain size at 259 cm
   ungroup() %>% 
   pivot_longer(D50, names_to = "group", values_to = "D50") %>% 
   mutate(year_bp_new = depth * (v2_C14$year/(v2_C14$depth_cm)),
          year_ce_new = 2017 - year_bp_new,
-         diff_time = lag(year_ce_new) - year_ce_new) # linear interpolation on AMS date
+         diff_time = lag(year_ce_new) - year_ce_new)  # linear interpolation on AMS date
 
 v2_percentages <- v2 %>% 
-  select(depth, year_bp, perc_clay:perc_sand) %>% 
+  filter(!`Record Number` %in% v2_tb) %>% 
+  select(depth, perc_clay:perc_sand) %>% 
   group_by(depth) %>% 
   summarize(across(perc_clay:perc_sand, mean)) %>%  # averge double samples that were done to confirm smaller grain size at 259 cm
   ungroup() %>% 
@@ -182,18 +217,42 @@ ggsave("figs/v2_D50_particlePercent.png",p,  width = 4, height = 8)
 
 ## Long plots Std. Departure
 
+# averages 
+v1.sd <- sd(v1$D50, na.rm = T)
+v1.mean <- mean(v1$D50, na.rm = T)
+
 v1.sd.fltr <- sd(v1_D50$D50, na.rm = T)
 v1.mean.fltr <- mean(v1_D50$D50, na.rm = T)
 
 v1_D50$stdep <- (v1_D50$D50 - v1.mean.fltr)/v1.sd.fltr
 
+v2.sd <- sd(v2$D50, na.rm = T)
+v2.mean <- mean(v2$D50, na.rm = T)
 
 v2.sd.fltr <- sd(v2_D50$D50, na.rm = T)
 v2.mean.fltr <- mean(v2_D50$D50, na.rm = T)
 
 v2_D50$stdep <- (v2_D50$D50 - v2.mean.fltr)/v2.sd.fltr
 
+core_stats <- readRDS('data/long_cores/core_stats.rds')
+
+
+gs_stats <- data.frame(
+  stat = rep(c('mean', 'mean_no_flood', 'sd', 'sd_no_flood'), 2),
+  value = c(v1.mean, v1.mean.fltr, v1.sd, v1.sd.fltr, v2.mean, v2.mean.fltr, v2.sd, v2.sd.fltr),
+  core = c(rep('V1', 4), rep("V2", 4)),
+  metric = rep('grain_size', 8)
+)
+
+core_stats_out <- rbind(core_stats, gs_stats)
+
+saveRDS(core_stats_out, 'data/long_cores/core_stats.rds')
 
 # plot on depth 
 v1_D50$depth <- v1_D50$depth * 10 
 v2_D50$depth <- v2_D50$depth * 10 
+
+grain_df <- rbind(v1_D50 %>% select(depth, year_ce_new, D50, stdep) %>% mutate(core = "V1"), 
+                  v2_D50 %>% select(depth, year_ce_new, D50, stdep) %>% mutate(core = "V2"))
+
+saveRDS(grain_df, 'data/long_cores/grain_size_v1_v2_combined.RDS')
